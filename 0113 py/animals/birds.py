0eab6021564dc7362a2f4f8f2ca51371905dc0b8
Mashiro2465 <rlaalstjr2465@gmail.com>
2026-01-13 19:07:46 +0900

    파이썬 과제 완료(2020-01-13)

---
+++ 0113 py/animals/birds.py
@@ -0,0 +1,7 @@
+class Eagle() :
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def info(self) :
+        return f"독수리 이름 : {self.name}, 나이 : {self.age}살" 
---
+++ 0113 py/mymath.py
@@ -0,0 +1,29 @@
+def 삼각형_넓이(x,h):
+    """
+    삼각형_넓이의 Docstring
+    
+    :param x: 가로
+    :param h: 높이
+    """
+
+    return x*h/2
+
+def 원의_넓이(r) :
+    """
+    원의_넓이의 Docstring
+    
+    :param r: 반지름
+    """
+
+    return (r*2)*3.14
+
+def 직육면체의_넓이(x,y,h):
+    """
+    직육면체의_넓이의 Docstring
+    
+    :param x: 가로
+    :param y: 세로
+    :param h: 높이
+    """
+
+    return 2*(x*h+y*h+x*y)
